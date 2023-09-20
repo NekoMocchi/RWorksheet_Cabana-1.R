d84f9810b5bfa719a0bc01c8eb5ac75fd6f81ee8
Anjelo Marco Cabana <anjelomarcobcabana@gmail.com>
2023-09-20 04:11:09 +0000

    R Worksheet 1

---
+++ RWorksheet_Cabana-1.R
@@ -0,0 +1,25 @@
+#1
+Age_Seq <- c("34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 51, 35, 24, 33, 41.")
+
+length(Age_Seq)
+
+#2
+reciprocalAge <- 1/Age_Seq
+reciprocalAge
+
+#3
+new_age <- c(Age_Seq, 0, Age_Seq).
+new.age
+
+#4
+SortedAge_Seq <- c(Age_Seq)
+SortedAge_Seq
+
+#5
+max(Age_Seq)
+min(Age_Seq)
+
+#6
+Age_Vec <- c(2.4, 2.8, 2.1, 2.5, 2.4 2.2, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, 2.7)
+             
+length(Age_Vec)
